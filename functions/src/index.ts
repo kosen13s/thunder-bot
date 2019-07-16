ec2ea561265b5c400007f6d898425af5c89efa15
Roadagain <ringoh72@gmail.com>
2019-07-16 22:20:01 +0900

    既存のものを参考にslackbotを作る

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import { App, ExpressReceiver } from '@slack/bolt'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,23 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((_, response) => {
   response.send("Hello from Firebase!");
 });
+
+// ref: https://github.com/seratch/bolt-on-cloud-functions-for-firebase
+
+const config = functions.config()
+
+const expressReceiver = new ExpressReceiver({
+  signingSecret: config.slack.secret,
+  endpoints: '/events'
+})
+
+const app = new App({
+  receiver: expressReceiver,
+  token: config.slack.bot.token
+})
+
+app.message('hello', ({ message, say }) => {
+  say(`Hello <@${message.user}>!`)
+})
+
+export const thunder = functions.https.onRequest(expressReceiver.app)
