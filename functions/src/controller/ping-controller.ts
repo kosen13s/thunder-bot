3303f0ec1d8021d10cd201e6cc3281d350086695
Roadagain <yamashiro@mfac.jp>
2019-07-22 14:01:53 +0900

    lintを忘れ去ってた

--- functions/src/controller/ping-controller.ts
+++ functions/src/controller/ping-controller.ts
@@ -1,11 +1,14 @@
 import { Middleware, SlackEventMiddlewareArgs } from '@slack/bolt'
 
-export const ping: Middleware<SlackEventMiddlewareArgs<'message'>> = ({ message, next }) => {
-    if (message.text === undefined) {
-        return
-    }
-    const pingPattern = /^ping$/i
-    if (pingPattern.exec(message.text.trim())) {
-        next()
-    }
+export const ping: Middleware<SlackEventMiddlewareArgs<'message'>> = ({
+  message,
+  next,
+}) => {
+  if (message.text === undefined) {
+    return
+  }
+  const pingPattern = /^ping$/i
+  if (pingPattern.exec(message.text.trim())) {
+    next()
+  }
 }
